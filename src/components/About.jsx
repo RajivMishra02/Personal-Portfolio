651829eddb0392cb4c7213ddc9ab613732b744eb
RajivMishra02 <78502868+RajivMishra02@users.noreply.github.com>
2026-07-15 17:52:48 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
       variants={fadeIn("","",0.1,1)}
       className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-      I'm a skilled software developer with experience in JavaScript and TypeScript, and expertise in frameworks like ReactJs, TailWind CSS and ThreeJs. I'm a quick learner and collaborate closely with fellow workers and clients to create efficient, scalable, and user-friendly solutions that solve real world problems. Let's work together to bring ideas to life!
+      I am software guy with my interest lying towards design, problem solving and product. I'm a quick learner and collaborate closely with fellow workers and clients to create efficient, scalable, and user-friendly solutions that solve real world problems. Let's work together to bring ideas to life!
       </motion.p>
       
       <div className='mt-20 flex flex-wrap gap-10'>
