46cafe1e2d9b650044579152609566231b8cacb1
RajivMishra02 <78502868+RajivMishra02@users.noreply.github.com>
2026-07-17 19:23:47 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Rajiv</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-        I am a Software Developer and I love to<br className='sm:block hidden'/>  develop interfaces and web applications
+        I am a Tech Nerd and I love to<br className='sm:block hidden'/>  brainstorm and solve problems.
         </p>
       </div>
 
