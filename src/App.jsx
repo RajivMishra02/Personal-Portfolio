d1adcaa09714d2c65917c5a66f63f39fbcc16a97
RajivMishra02 <78502868+RajivMishra02@users.noreply.github.com>
2026-07-17 19:29:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas} from "./components"
+import {About, Contact, Experience, Hero, Navbar, Tech, StarsCanvas} from "./components"
 
 const App = () => {
   
@@ -15,7 +15,7 @@ const App = () => {
         <About />
         <Experience />
         <Tech />
-        <Works />
+        {/* <Works />  */}
         
         <div className="relative z-0">
           <Contact />
