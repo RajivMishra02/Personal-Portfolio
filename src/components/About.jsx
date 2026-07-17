6e158f79826557bd42437b00e20c89cd392d1e4f
RajivMishra02 <78502868+RajivMishra02@users.noreply.github.com>
2026-07-17 19:16:32 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
       variants={fadeIn("","",0.1,1)}
       className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-      I am software guy with my interest lying towards design, problem solving and product. I'm a quick learner and collaborate closely with fellow workers and clients to create efficient, scalable, and user-friendly solutions that solve real world problems. Let's work together to bring ideas to life!
+      I am a software guy with my interest lying towards design, problem solving and product. I'm a quick learner and collaborate closely with fellow workers and clients to create efficient, scalable, and user-friendly solutions that solve real world problems. Let's work together to bring ideas to life!
       </motion.p>
       
       <div className='mt-20 flex flex-wrap gap-10'>
